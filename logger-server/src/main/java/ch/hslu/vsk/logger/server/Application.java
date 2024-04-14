f99b5f6f6687528f6453f8209d883892f098160f
Yannick Frischknecht <yannick.frischknecht@basenet.ch>
2024-04-14 23:48:35 +0200

    feat: #7 implements ConfigFileReader to read config for LoggerServer

--- logger-server/src/main/java/ch/hslu/vsk/logger/server/Application.java
+++ logger-server/src/main/java/ch/hslu/vsk/logger/server/Application.java
@@ -1,15 +1,18 @@
 package ch.hslu.vsk.logger.server;
 
+import ch.hslu.vsk.logger.common.ConfigFileReader;
 import ch.hslu.vsk.stringpersistor.api.StringPersistor;
 import ch.hslu.vsk.stringpersistor.impl.StringPersistorFactory;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.nio.file.Path;
+import java.util.Properties;
 
 public class Application {
-    public static void main(String[] args) throws JsonProcessingException {
-        StringPersistor stringPersistor = StringPersistorFactory.create(Path.of("log.txt"));
-        LoggerServer loggerServer = new LoggerServer("tcp://localhost:5555", stringPersistor);
+    public static void main(String[] args) {
+        Properties prop = ConfigFileReader.Read(Path.of("app.config"));
+
+        StringPersistor stringPersistor = StringPersistorFactory.create(Path.of(prop.getProperty("logFilePath")));
+        LoggerServer loggerServer = new LoggerServer(prop.getProperty("url") + ":" + prop.getProperty("port"), stringPersistor);
         loggerServer.start();
     }
 }
