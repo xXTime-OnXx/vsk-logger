c1e78bb067177d125ee823528c4fd43ad2314845
Maiko Trede <maikotrede@gmail.com>
2024-04-14 23:33:24 +0200

    docker implementation

--- logger-server/src/main/java/ch/hslu/vsk/logger/server/Application.java
+++ logger-server/src/main/java/ch/hslu/vsk/logger/server/Application.java
@@ -2,12 +2,11 @@ package ch.hslu.vsk.logger.server;
 
 import ch.hslu.vsk.stringpersistor.api.StringPersistor;
 import ch.hslu.vsk.stringpersistor.impl.StringPersistorFactory;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.nio.file.Path;
 
 public class Application {
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void main(String[] args) {
         StringPersistor stringPersistor = StringPersistorFactory.create(Path.of("log.txt"));
         LoggerServer loggerServer = new LoggerServer("tcp://localhost:5555", stringPersistor);
         loggerServer.start();
